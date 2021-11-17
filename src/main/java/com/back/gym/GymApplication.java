29fe36038ef7245b4349e1c1865c29d517181f4b
Manuel Martinez <manuelmartinezingb@gmail.com>
2021-11-16 19:29:24 -0500

    Manuel modify 1

--- src/main/java/com/back/gym/GymApplication.java
+++ src/main/java/com/back/gym/GymApplication.java
@@ -2,7 +2,7 @@ package com.back.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hola
 @SpringBootApplication
 public class GymApplication {
 
